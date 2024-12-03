748fa01c704d8c6a7a7b32cebb1eda2a60d187ab
sheheryar810 <sheheryar810@gmail.com>
2024-12-04 00:03:47 +0300

    Profile Route Updated

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -5,6 +5,10 @@ const { authenticate } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', registerProfile);
+
+// if we want to add middleware authentication for registration
+// router.post('/register', authenticate, registerProfile); 
+
 router.get('/match', authenticate, matchProfiles);
 
 module.exports = router;
